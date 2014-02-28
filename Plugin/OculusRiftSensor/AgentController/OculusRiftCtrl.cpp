ec067f405bf301ee8e5cfcd6643e3992f619d11f
Tetsunari Inamura <inamura@nii.ac.jp>
2014-03-01 06:45:58 +0900

    correct fluctuation of description

--- Plugin/OculusRiftSensor/AgentController/OculusRiftCtrl.cpp
+++ Plugin/OculusRiftSensor/AgentController/OculusRiftCtrl.cpp
@@ -1,161 +1,154 @@
-#include <Controller.h>  
-#include <ControllerEvent.h>  
-#include <Logger.h>  
-#include <ViewImage.h>  
-#include <math.h>  
-#include <stdio.h>  
-#include <stdlib.h>  
+#include <Controller.h>
+#include <ControllerEvent.h>
+#include <Logger.h>
+#include <ViewImage.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define PI 3.141592  
-#define DEG2RAD(DEG) ( (PI) * (DEG) / 180.0 )  
-  
-class RobotController : public Controller  
-{  
-public:  
-  void onRecvMsg(RecvMsgEvent &evt);  
-  void onInit(InitEvent &evt);  
-  double onAction(ActionEvent &evt);  
-private:   
-  
-  BaseService *m_kinect;
-  BaseService *m_hmd;
-  
-  double m_posx, m_posy, m_posz;
-  double m_yrot;
-  double m_range;
+#define DEG2RAD(DEG) ( (M_PI) * (DEG) / 180.0 )
 
-  int m_maxsize;
-  
-  double m_qw, m_qy, m_qx, m_qz;
-
-  double pyaw, ppitch, proll;
-
-  bool chk_srv;
-};   
-
-void RobotController::onInit(InitEvent &evt)  
-{  
-  srand(time(NULL));  
-
-  SimObj *my = this->getObj(this->myname());
-  m_posx = my->x();
-  m_posy = my->y();
-  m_posz = my->z();
-  m_range = 0.1;
-  m_maxsize = 15;
-  double qw = my->qw();
-  double qy = my->qy();
-  m_yrot = acos(fabs(qw))*2;
-  if(qw*qy > 0)
-    m_yrot = -1*m_yrot;
-
-  m_qw = 1.0;
-  m_qx = 0.0;
-  m_qy = 0.0;
-  m_qz = 0.0;
-
-  pyaw = ppitch = proll = 0.0;
-}    
-  
-double RobotController::onAction(ActionEvent &evt)  
+class RobotController : public Controller
 {
-  if(chk_srv==NULL){
-    bool ch_kinect = checkService("SIGORS");
+public:
+	void onRecvMsg(RecvMsgEvent &evt);
+	void onInit(InitEvent &evt);
+	double onAction(ActionEvent &evt);
+private:
+	BaseService *m_kinect;
+	BaseService *m_hmd;
+	double m_posx, m_posy, m_posz;
+	double m_yrot;
+	double m_range;
 
-    if(ch_kinect){
-      m_kinect = connectToService("SIGORS");
-    }
-  }
+	int m_maxsize;
 
-  return 1.0;  
-}   
+	double m_qw, m_qy, m_qx, m_qz;
 
-void RobotController::onRecvMsg(RecvMsgEvent &evt)  
-{  
+	double pyaw, ppitch, proll;
 
-  std::string sender = evt.getSender();  
-  
-  SimObj *my = getObj(myname());  
-  
-  char *all_msg = (char*)evt.getMsg();  
+	bool chk_srv;
+};
 
-  std::string ss = all_msg;
-  int strPos1 = 0;
-  int strPos2;
-  std::string headss;
-  std::string tmpss;
-  strPos2 = ss.find(" ", strPos1);
-  headss.assign(ss, strPos1, strPos2-strPos1);
+void RobotController::onInit(InitEvent &evt)
+{
+	srand(time(NULL));
 
-  if(headss == "ORS_DATA"){
-//    LOG_MSG((all_msg));
-//  }
-//  if(headss == "HMD_DATA"){
+	SimObj *my = this->getObj(this->myname());
+	m_posx = my->x();
+	m_posy = my->y();
+	m_posz = my->z();
+	m_range = 0.1;
+	m_maxsize = 15;
+	double qw = my->qw();
+	double qy = my->qy();
+	m_yrot = acos(fabs(qw))*2;
+	if(qw*qy > 0)
+		m_yrot = -1*m_yrot;
 
-    double yaw, pitch, roll;
-    strPos1 = strPos2+1;
-    tmpss = "";
+	m_qw = 1.0;
+	m_qx = 0.0;
+	m_qy = 0.0;
+	m_qz = 0.0;
 
-    strPos2 = ss.find(",", strPos1);
-    tmpss.assign(ss, strPos1, strPos2-strPos1);
-    yaw = atof(tmpss.c_str());
-
-    strPos1 = strPos2+1;
-    strPos2 = ss.find(",", strPos1);
-    tmpss.assign(ss, strPos1, strPos2-strPos1);
-    pitch = atof(tmpss.c_str());
-
-    strPos1 = strPos2+1;
-    strPos2 = ss.find(",", strPos1);
-    tmpss.assign(ss, strPos1, strPos2-strPos1);
-    roll = atof(tmpss.c_str());
-
-    if(yaw == pyaw && pitch == ppitch && roll == proll)  return;
-    else {
-      pyaw = yaw;
-      ppitch = pitch;
-      proll = roll;
-    }
-
-    dQuaternion qyaw;
-    dQuaternion qpitch;
-    dQuaternion qroll;
-
-    qyaw[0] = cos(-yaw/2.0);
-    qyaw[1] = 0.0;
-    qyaw[2] = sin(-yaw/2.0);
-    qyaw[3] = 0.0;
-
-    qpitch[0] = cos(-pitch/2.0);
-    qpitch[1] = sin(-pitch/2.0);
-    qpitch[2] = 0.0;
-    qpitch[3] = 0.0;
-
-    qroll[0] = cos(-roll/2.0);
-    qroll[1] = 0.0;
-    qroll[2] = 0.0;
-    qroll[3] = sin(-roll/2.0);
-    dQuaternion tmpQ1;
-    dQuaternion tmpQ2;
-
-    dQMultiply0(tmpQ1, qyaw, qpitch);
-    dQMultiply0(tmpQ2, tmpQ1, qroll);
-
-    dQuaternion bodyQ;
-    bodyQ[0] = m_qw;
-    bodyQ[1] = m_qx;
-    bodyQ[2] = m_qy;
-    bodyQ[3] = m_qz;
-
-    dQuaternion tmpQ3;
-    dQMultiply1(tmpQ3, bodyQ, tmpQ2);
-
-    my->setJointQuaternion("HEAD_JOINT0", tmpQ3[0], tmpQ3[1], tmpQ3[2], tmpQ3[3]);
-  }
+	pyaw = ppitch = proll = 0.0;
 }
 
-extern "C" Controller * createController ()  
-{  
-  return new RobotController;  
-}  
+double RobotController::onAction(ActionEvent &evt)
+{
+	if(chk_srv==NULL) {
+		bool ch_kinect = checkService("SIGORS");
 
+		if(ch_kinect) {
+			m_kinect = connectToService("SIGORS");
+		}
+	}
+
+	return 1.0;
+}
+
+void RobotController::onRecvMsg(RecvMsgEvent &evt)
+{
+	std::string sender = evt.getSender();
+	SimObj *my = getObj(myname());
+	char *all_msg = (char*)evt.getMsg();
+
+	std::string ss = all_msg;
+	int strPos1 = 0;
+	int strPos2;
+	std::string headss;
+	std::string tmpss;
+	strPos2 = ss.find(" ", strPos1);
+	headss.assign(ss, strPos1, strPos2-strPos1);
+
+	if(headss == "ORS_DATA") {
+		//    LOG_MSG((all_msg));
+		//  }
+		//  if(headss == "HMD_DATA"){
+
+		double yaw, pitch, roll;
+		strPos1 = strPos2+1;
+		tmpss = "";
+
+		strPos2 = ss.find(",", strPos1);
+		tmpss.assign(ss, strPos1, strPos2-strPos1);
+		yaw = atof(tmpss.c_str());
+
+		strPos1 = strPos2+1;
+		strPos2 = ss.find(",", strPos1);
+		tmpss.assign(ss, strPos1, strPos2-strPos1);
+		pitch = atof(tmpss.c_str());
+
+		strPos1 = strPos2+1;
+		strPos2 = ss.find(",", strPos1);
+		tmpss.assign(ss, strPos1, strPos2-strPos1);
+		roll = atof(tmpss.c_str());
+
+		if(yaw == pyaw && pitch == ppitch && roll == proll)  return;
+		else {
+			pyaw = yaw;
+			ppitch = pitch;
+			proll = roll;
+		}
+
+		dQuaternion qyaw;
+		dQuaternion qpitch;
+		dQuaternion qroll;
+
+		qyaw[0] = cos(-yaw/2.0);
+		qyaw[1] = 0.0;
+		qyaw[2] = sin(-yaw/2.0);
+		qyaw[3] = 0.0;
+
+		qpitch[0] = cos(-pitch/2.0);
+		qpitch[1] = sin(-pitch/2.0);
+		qpitch[2] = 0.0;
+		qpitch[3] = 0.0;
+
+		qroll[0] = cos(-roll/2.0);
+		qroll[1] = 0.0;
+		qroll[2] = 0.0;
+		qroll[3] = sin(-roll/2.0);
+		dQuaternion tmpQ1;
+		dQuaternion tmpQ2;
+
+		dQMultiply0(tmpQ1, qyaw, qpitch);
+		dQMultiply0(tmpQ2, tmpQ1, qroll);
+
+		dQuaternion bodyQ;
+		bodyQ[0] = m_qw;
+		bodyQ[1] = m_qx;
+		bodyQ[2] = m_qy;
+		bodyQ[3] = m_qz;
+
+		dQuaternion tmpQ3;
+		dQMultiply1(tmpQ3, bodyQ, tmpQ2);
+
+		my->setJointQuaternion("HEAD_JOINT0", tmpQ3[0], tmpQ3[1], tmpQ3[2], tmpQ3[3]);
+	}
+}
+
+extern "C" Controller * createController ()
+{
+	return new RobotController;
+}
